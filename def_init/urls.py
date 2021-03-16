e89260b67d37e79e42e071f3dccab6ec92b90ba8
sota0829 <souta0829win7@yahoo.co.jp>
2021-03-17 00:50:25 +0900

    add iroiro

--- def_init/urls.py
+++ def_init/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+import def_i.views as def_i #いる？
+from django.contrib.staticfiles.urls import static,staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('def_i.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
