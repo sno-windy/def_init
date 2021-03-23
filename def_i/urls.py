7b7aa9069da981ee596f3c985d7486d81273044f
sota0829 <souta0829win7@yahoo.co.jp>
2021-03-23 22:47:50 +0900

    モデル少し変更したのでmigrateお願いします，talk投稿が反映されるようになった．自分のアイコンがクリックできる

--- def_i/urls.py
+++ def_i/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('article_feed_like/',views.ArticleFeedLike.as_view(), name='article_feed_like'),
     path('article_detail/<int:pk>/',views.ArticleDetail.as_view(),name="article_detail"),
     path('article_talk/<int:pk>/',views.ArticleTalk.as_view(),name="article_talk"),
+    path('article_talk_suc/<int:pk>/',views.ArticleTalkSuc.as_view(),name='article_talk_suc'),
 ]
