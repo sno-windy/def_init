06f81b135e20f1cf6a376b62b5d6178688f85151
sota0829 <souta0829win7@yahoo.co.jp>
2021-03-25 19:00:05 +0900

    question周辺の基幹作成

--- def_i/views.py
+++ def_i/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import (
 from django.views.generic import ListView,DetailView,FormView,TemplateView,CreateView
 from django.views.generic.edit import FormMixin
 from .forms import LoginForm, ArticleTalkForm, ArticlePostForm
-from .models import User,Article,TalkAtArticle
+from .models import User,Article,TalkAtArticle,Question,TalkAtQuestion
 
 def index(request):
     return render(request,"def_i/index.html")
@@ -81,12 +81,45 @@ class ArticlePost(CreateView):
         print(initial['poster'])
         return initial
 
-class Login(LoginView):
-    """ログインページ"""
-    form_class = LoginForm
-    template_name = 'def_i/login.html'
+class QuestionFeed(ListView):
+    model = Question
+    context_object_name = "questions"
+    template_name = "def_i/question_feed.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        questions = Article.objects.order_by('-created_at')
+        return questions
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sort_by_new'] = True
+        return context
+
+class QuestionFeedUnanswered(QuestionFeed):
+    def get_queryset(self): #queryset=だけでいいことが判明
+        articles = Question.objects.filter(if_answered=False)
+        return articles
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sort_by_new'] = False
+        return context
+
+class QuestionDetail(DetailView):
+    model = Question
+    context_object_name = "contents"
+    template_name = "def_i/question_detail.html"
 
 
-class Logout(LogoutView):
-    """ログアウトページ"""
-    template_name = 'def_i/top.html'
+
+
+# class Login(LoginView):
+#     """ログインページ"""
+#     form_class = LoginForm
+#     template_name = 'def_i/login.html'
+
+
+# class Logout(LogoutView):
+#     """ログアウトページ"""
+#     template_name = 'def_i/top.html'
