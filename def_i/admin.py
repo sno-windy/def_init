053416ffdb8aaf7f35332d5f808bf04162411ecb
才木涼介 <ryosukekekeke0625@gmail.com>
2021-04-03 13:50:08 +0900

    models追加

--- def_i/admin.py
+++ def_i/admin.py
@@ -1,10 +1,12 @@
-from .models import  Article, Question, Like, Task, Talk, TalkAtArticle, TalkAtQuestion
+from .models import  Article, Question, Like, Task, Talk, TalkAtArticle, TalkAtQuestion, Memo, Task_Sub
 from django.contrib import admin
 
 admin.site.register(Article)
 admin.site.register(Question)
 admin.site.register(Like)
 admin.site.register(Task)
+admin.site.register(Task_Sub)
 admin.site.register(Talk)
 admin.site.register(TalkAtQuestion)
 admin.site.register(TalkAtArticle)
+admin.site.register(Memo)
