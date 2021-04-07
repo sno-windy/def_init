8732622c7d04cedcacb6e8b49553968fa5867787
sota0829 <souta0829win7@yahoo.co.jp>
2021-04-07 10:01:37 +0900

    boto3WIP

--- def_init/settings.py
+++ def_init/settings.py
@@ -196,6 +196,11 @@ except ImportError:
     pass
 
 #AWS_S3
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 AWS_STORAGE_BUCKET_NAME = 'def-init'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'def_init.storage_backends.MediaStorage'  # <-- here is where we reference it
