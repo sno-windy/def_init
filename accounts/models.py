db6967810102ccc9206b7f7be4c87b9ff934ca57
sota0829 <souta0829win7@yahoo.co.jp>
2021-04-26 01:11:34 +0900

    不具合修正

--- accounts/models.py
+++ accounts/models.py
@@ -10,5 +10,6 @@ class User(AbstractUser):
     user_image = ProcessedImageField(upload_to="accounts/img",
         processors=[ResizeToFill(250,250)],
         format='JPEG',
-        options={'quality':60}
+        options={'quality':60},
+        blank=True
         )
