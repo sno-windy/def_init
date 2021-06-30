e795689e49ffe105f64d5bef5362bbc0e0408781
sota0829 <souta0829win7@yahoo.co.jp>
2021-07-01 02:40:16 +0900

    logging

--- def_init/settings/production.py
+++ def_init/settings/production.py
@@ -27,3 +27,49 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 AWS_STORAGE_BUCKET_NAME = 'demia-def-init'
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+
+ADMINS = ("Higaki", "souta0829win7@yahoo.co.jp")
+
+SESSION_COOKIE_SECURE = True
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            "datefmt": "%d/%b/%Y %H:%M:%S",
+        },
+    },
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+    },
+    "handlers": {
+        "null": {
+            "level": "DEBUG",
+            "class": "logging.NullHandler",
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "/var/log/def-init/debug.log",  # パスは環境に合わせて、ファイルは作る
+            "maxBytes": 1024 * 1024 * 512,
+            "backupCount": 10,
+            "formatter": "standard",
+        },
+        "console": {"level": "INFO", "class": "logging.StreamHandler", "formatter": "standard"},
+        "mail_admins": {
+            "level": "ERROR",
+            "class": "django.utils.log.AdminEmailHandler",
+            "include_html": True,
+            "filters": ["require_debug_false"],
+        },
+    },
+    "loggers": {
+        "django.security.DisallowedHost": {"handlers": ["null"], "propagate": False},
+        "django": {"handlers": ["file", "console", "mail_admins"], "level": "DEBUG", "propagate": True,},  # NOQA: E231
+        "main": {"handlers": ["file", "console", "mail_admins"], "level": "DEBUG", "propagate": True,},  # NOQA: E231
+    },
+}
